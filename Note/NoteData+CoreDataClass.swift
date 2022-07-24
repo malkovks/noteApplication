3c9614b4f3150bae7ac635d8407b80ba4a59204e
Константин Малков <70747233+kokmalkok@users.noreply.github.com>
2022-07-24 10:59:31 +0300

    Add files via upload

---
+++ Note/NoteData+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  NoteData+CoreDataClass.swift
+//  Note
+//
+//  Created by Константин Малков on 30.05.2022.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(NoteData)
+public class NoteData: NSManagedObject {
+
+}
